958806b8600bb744d195672ad59038cba91b746e
Jason Gonzales <gjasonaaron@gmail.com>
2020-03-24 18:42:33 +0800

    release v1 patch 2

---
+++ precache-manifest.518b7793c962d105a47df2b6e31cdabf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d791ad1fb360f04dc9f60424ccd143b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cdb2edf133db75fb0d1",
+    "url": "./static/css/main.38c82d8e.chunk.css"
+  },
+  {
+    "revision": "41ae8e8e29e5d2c0c624",
+    "url": "./static/js/2.9fe3d40f.chunk.js"
+  },
+  {
+    "revision": "60037e8887f425d039bd63775efa9319",
+    "url": "./static/js/2.9fe3d40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cdb2edf133db75fb0d1",
+    "url": "./static/js/main.e1f308ae.chunk.js"
+  },
+  {
+    "revision": "f9e6a427da4c57a86023",
+    "url": "./static/js/runtime-main.8b807a4f.js"
+  },
+  {
+    "revision": "1f0d26334cff586b6b935d73d000b863",
+    "url": "./static/media/esports-port.1f0d2633.jpg"
+  },
+  {
+    "revision": "bf0a19550d4edff00635a867094602db",
+    "url": "./static/media/jwservice-port.bf0a1955.jpg"
+  },
+  {
+    "revision": "5055f9f6732b3e8442d1d0ccb7a4bddf",
+    "url": "./static/media/portfolio.5055f9f6.mp4"
+  },
+  {
+    "revision": "12963638462b94fd42331311dac2d1c9",
+    "url": "./static/media/tzilla-port.12963638.jpg"
+  }
+]);
